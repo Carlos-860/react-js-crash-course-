2616fe102617f7df7dbf6d37bcc0d26193686687
Carlos-860 <carlostimotheus5@gmail.com>
2024-04-26 12:37:35 +0200

    Fetch an API to Display on Page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Header from "./components/Header";
 import Employees from "./pages/Employees";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Customers from "./pages/Customers";
-import Dictionary from "./components/Dictionary";
+import Dictionary from "./pages/Dictionary";
+import Definition from "./pages/Definition";
 
 function App() {
   return (
@@ -16,11 +17,10 @@ function App() {
       <Header>
         <Routes>
           <Route path="/employees" element={<Employees />} />
-          
+        
           <Route path="/customers" element={<Customers />} />
-
           <Route path="/dictionary" element={<Dictionary />} />
-
+          <Route path="/definition" element={<Definition />} />
         </Routes>
       </Header>
     </BrowserRouter>
