295c894da33dab1adc40772f01d947ccac2b8f1e
Carlos-860 <carlostimotheus5@gmail.com>
2024-04-27 10:30:07 +0200

    Build and Style a Search Component

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -2,24 +2,44 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import NotFound from "../components/NotFound";
+import DefinitionSearch from "../components/DefinitionSearch";
 
 export default function Definition() {
   const [word, setWord] = useState([]);
   const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
+
   let { search } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
+    // const url = "https://httpstat.us/501";
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`;
+
+    fetch(url)
       .then((response) => {
         console.log(response.status);
         if (response.status === 404) {
           setNotFound(true);
+        } else if (response.status === 401) {
+          navigate("/login");
+        } else if (response.status === 500) {
+            // setServerError(true);
+        }
+
+        // Catch bogus urls and throw and error
+        if (!response.ok) {
+          setError(true);
+
+          throw new Error("Something went wrong");
         }
         return response.json();
       })
       .then((data) => {
         setWord(data[0].meanings);
+      })
+      .catch((e) => {
+        console.log(e.message);
       });
   }, []);
 
@@ -27,6 +47,15 @@ export default function Definition() {
     return (
       <>
         <NotFound />
+        <Link to="/dictionary">Search another</Link>
+      </>
+    );
+  }
+
+  if (error === true) {
+    return (
+      <>
+        <p>Something went wrong, try again?</p>
         <Link to="/dictionary">Search another</Link>
       </>
     );
@@ -45,6 +74,9 @@ export default function Definition() {
               </p>
             );
           })}
+
+          <p>Search again:</p>
+          <DefinitionSearch />
         </>
       ) : null}
     </>
